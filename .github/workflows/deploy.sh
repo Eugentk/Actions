f4e6a058035ddcf290303da03c237fd0081d9923
Eugen Tkachenko <y.tkachenko@mobidev.biz>
2021-06-16 12:40:43 +0300

    new change2

--- .github/workflows/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo 'Starting to Deploy...'
-ssh ubuntu@34.241.248.244 " sudo docker image prune -f
-        cd poster
-        sudo docker-compose down
-        sudo docker-compose up --build -d --force-recreate
-        "
-echo 'Deployment completed successfully'
